e9d2bc77ed42029ff128604db25dcd586cb9c43f
ederim1 <ederim@byui.edu>
2022-09-17 23:45:25 -0600

    js added

--- course_home_page/scripts/main.js
+++ course_home_page/scripts/main.js
@@ -2,4 +2,6 @@ let date = new Date();
 let year = date.getFullYear();
 
 document.querySelector('.year').textContent = year;
+let text = document.lastModified;
+document.getElementById("updated").innerHTML = text;
 
