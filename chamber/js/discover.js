2d7a0ca920318f09aa212322c89016174540004a
ederim1 <ederim@byui.edu>
2022-10-30 02:36:05 -0600

    discover page

--- chamber/js/discover.js
+++ chamber/js/discover.js
@@ -30,3 +30,36 @@ function setStyles() {
     localStorage.setItem('lastvisit', Date.now());
 }
     
+/////   Images Lazy /////
+const image = document.querySelectorAll('img');
+
+const pimages = document.querySelectorAll('[data-src]');
+const options = {
+threshold: 1,
+rootMargin: "0px 0px 100px 0px"
+}
+
+function preloadImage(img){
+const source = img.getAttribute('data-src');
+if (!source) {
+return;
+}
+
+img.src = source;
+}
+
+const io = new IntersectionObserver (
+(entries, io) => {
+entries.forEach(entry => {
+if (!entry.isIntersecting) {
+return;
+} else {
+preloadImage(entry.target);
+io.unobserve(entry.target);
+}
+});
+},options);
+
+pimages.forEach(image => {
+    io.observe(image);
+});
