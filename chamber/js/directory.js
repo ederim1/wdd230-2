27d9cfaf4c79cce69b6206ec4c651536e9a899fe
ederim1 <ederim@byui.edu>
2022-11-11 22:43:19 -0700

    business info added

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -1,8 +1,9 @@
 const requestDirectory = 'https://ederim1.github.io/wdd230-2/chamber/js/data.json';
 
+let businesses = [];
 
 
-async function getDirectory(requestDirectory) {
+async function loadDirectory(requestDirectory) {
 
     const response = await fetch(requestDirectory);
 
@@ -14,23 +15,20 @@ async function getDirectory(requestDirectory) {
 
         console.log(jsObject);
 
-        const businesses = jsObject['businesses'];
+        businesses = jsObject['businesses'];
 
-        console.log(businesses[0].lastname);
+        businesses.forEach(createBusinessCard);
+        businesses.forEach(createBusinessRows);
 
-        businesses.forEach(displayDirectory);
-
+    
     }
 
-}
+} 
+
+loadDirectory(requestDirectory);
 
 
-
-getDirectory(requestDirectory);
-
-
-
-function displayDirectory(item) {
+function createBusinessCard(item) {
 
     let card = document.createElement('section');
 
@@ -43,8 +41,6 @@ function displayDirectory(item) {
     let a = document.createElement('a')
 
     let img = document.createElement('img');
-
-
 
     h2.textContent = item.name; 
 
@@ -60,15 +56,69 @@ function displayDirectory(item) {
 
     img.setAttribute('alt', item.name);
 
-
     card.appendChild(h2);
 
     card.appendChild(p1); 
 
-    card.appendChild(p2); 
+    card.appendChild(p2);
+    
+    card.appendChild(a);
 
-    card.appendChild(img); 
-
+    card.appendChild(img);
+    
     document.querySelector('.cards').appendChild(card); 
 
 }
+
+function createBusinessRows(item) {
+
+    let tableRow = document.createElement('tr');
+
+    let td1 = document.createElement('td');
+
+    let td2 = document.createElement('td'); 
+
+    let td3 = document.createElement('td'); 
+
+    let td4 = document.createElement('td');
+
+    let a = document.createElement('a');
+
+    td1.textContent = item.name; 
+
+    td2.textContent = 'Address: ' + item.address ;
+
+    td3.textContent = ' phone: ' + item.phone;
+    
+    a.textContent = "url: " + item.websiteurl;
+
+    a.setAttribute('href', item.websiteurl);
+
+    td4.appendChild(a);
+
+    tableRow.appendChild(td1);
+
+    tableRow.appendChild(td2); 
+
+    tableRow.appendChild(td3); 
+
+    tableRow.appendChild(td4); 
+
+    document.querySelector('.directoryTable').appendChild(tableRow); 
+
+}
+
+
+function displayDirectory() {
+    document.querySelector(".cards").classList.remove("hidden");
+    document.querySelector(".directoryTable").classList.add("hidden");
+}
+
+function displayTable() {
+    document.querySelector(".directoryTable").classList.remove("hidden");
+    document.querySelector(".cards").classList.add("hidden")
+}
+
+document.querySelector("#directory").addEventListener("click", displayDirectory);
+document.querySelector("#table").addEventListener("click", displayTable);
+
